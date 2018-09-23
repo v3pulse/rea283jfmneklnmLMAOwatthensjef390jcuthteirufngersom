c7c80da67eb898c745318a72b7136258e38318cb
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-23 17:24:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ function generateMessages(){
 
 
 bot.on("message", message => {
-    let owner = message.guild.roles.find(r => r.name === "Management");
     if(!message.channel.id === "493532426530848778") return;
-    if (message.member.roles.has(owner)) && message.content.toLowerCase() == setupCMD){
+    if (message.member.roles.has(message.guild.roles.find(r => r.name === "Management")) && message.content.toLowerCase() == setupCMD){
         var toSend = generateMessages();
         let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
         for (let mapObj of mappedArray){
