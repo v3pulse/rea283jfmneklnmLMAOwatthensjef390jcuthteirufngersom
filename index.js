87abb530e6126803033c5c5f4ef922ac5a0ee90e
v3pulse <43049519+v3pulse@users.noreply.github.com>
2018-09-23 17:14:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (roles.length !== reactions.length) throw "Roles list and reactions list are 
 function generateMessages(){
     var messages = [];
     messages.push(initialMessage);
-    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`);
+    for (let role of roles) messages.push(`React below set your region to **"${role}"**`);
     return messages;
 }
 
